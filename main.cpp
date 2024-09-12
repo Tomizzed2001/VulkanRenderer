216486273604f22d76d03e4deb45911a58a2fbd5
Tom Jackson <tom.jackson1711@gmail.com>
2024-09-12 15:35:11 +0100

    Memory Allocator
    Added the code necessary to create a vulkan memory allocator.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
+#define VMA_IMPLEMENTATION
+#include <vk_mem_alloc.h>
+
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
@@ -12,18 +15,22 @@
 #include "setup.hpp"
 
 namespace {
-
-
+    /// <summary>
+    /// Creates a memory allocator using the vma library for Vulkan
+    /// memory allocation.
+    /// </summary>
+    /// <param name="app">The context of the application</param>
+    /// <returns>Vulkan Memory Allocator</returns>
+    VmaAllocator createMemoryAllocator(app::AppContext& app);
 }
 
 int main() {
     try {
         // -- The setup -- //
-
         app::AppContext application = app::setup();
 
-        // -- The setup -- //
-
+        // Create the memory allocator
+        VmaAllocator allocator = createMemoryAllocator(application);
 
         // Main render loop
         while (!glfwWindowShouldClose(application.window)) {
@@ -44,5 +51,27 @@ int main() {
 }
 
 namespace {
+    VmaAllocator createMemoryAllocator(app::AppContext& app) {
+        VkPhysicalDeviceProperties properties{};
+        vkGetPhysicalDeviceProperties(app.physicalDevice, &properties);
 
+        VmaVulkanFunctions functions{};
+        functions.vkGetInstanceProcAddr = vkGetInstanceProcAddr;
+        functions.vkGetDeviceProcAddr = vkGetDeviceProcAddr;
+
+        VmaAllocatorCreateInfo allocInfo{};
+        allocInfo.vulkanApiVersion = properties.apiVersion;
+        allocInfo.physicalDevice = app.physicalDevice;
+        allocInfo.device = app.logicalDevice;
+        allocInfo.instance = app.instance;
+        allocInfo.pVulkanFunctions = &functions;
+
+        VmaAllocator allocator = VK_NULL_HANDLE;
+        if (vmaCreateAllocator(&allocInfo, &allocator) != VK_SUCCESS)
+        {
+            throw std::runtime_error("Failed to create VmaAllocator");
+        }
+
+        return allocator;
+    }
 }
